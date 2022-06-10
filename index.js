ad7d3f9d20cf83e27bb6e9c261573771b6e98b17
maxkram <max.kramer@live.com>
2022-06-10 16:18:16 -0400

    feature: add up

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ const fileManager = async () => {
                 break;
             }
             case 'ls': {
+                
                 break;
             }
             case 'cat': {
